b5bd6e5ad88b4a7757fdd40b0ca88ebacf4d658b
zhiwuwu <510136920@qq.com>
2016-10-30 14:25:43 +0800

    meizu hover

--- js/meizu.js
+++ js/meizu.js
@@ -8,7 +8,7 @@ function getStyle(obj,name){
 }
 // 获取class
 function getByClass(oParent,nClass) {
-	// 获取Tag 
+	// 获取Tag
 	var ele = oParent.getElementsByTagName('*');
 	// 创建一个空数组
 	var aRrent =[];
@@ -22,38 +22,46 @@ function getByClass(oParent,nClass) {
 }
 
 //动作函数
-function startMove(obj,att,add) {
-	// 清除定时器
-	clearInterval(obj.timer);
-	obj.timer = setInterval(function () {
-		// 存储当前样式的值
-		var cutt = 0 ;
-		if(att == 'opacity'){
-			//正常浏览器
-			cutt = Math.round(parseFloat(getStyle(obj,att)));
-		}else{
-			// ie
-			cutt = Math.round(parseInt(getStyle(obj,att)));
+//同时运动函数（对象，{属性:目标值,}，速度，下一运动）
+function starMove(obj,json,spee,fn) {
+var flag = true ;
+clearInterval(obj.timer);//清理一下防止重复调用计时器
+obj.timer = setInterval(function() {
+for(var attr in json)
+{
+	//1.获取属性值
+	var icur ;
+	if (attr == 'opacity') {
+		icur = Math.round(parseFloat(getStyle(obj,attr))*100);
+	} else {
+		icur = parseInt(getStyle(obj,attr));
+	}
+	//2.算速度
+	var speed = (json[attr] - icur)/spee;
+	speed = speed > 0? Math.ceil(speed) : Math.floor(speed);
+	//3.检测停止
+	if (icur != json[attr]){
+		flag = false ;
+	}
+		if (attr == 'opacity') {
+		obj.style.filter = 'alpha(opacity:'+(icur+speed)+')';
+		obj.style.opacity = (icur+speed)/100;
+		} else {
+			obj.style[attr] = icur+speed+'px';
 		}
-		// 设置速度
-		var speed = (add-cutt)/3;
-		// 三维判断使之变成整数
-		speed = speed>0?Math.ceil(speed):Math.floor(speed);
-		if (cutt==add) {
-			clearInterval(obj.timer)
-		}else{
-			if(att=='opacity'){
-				obj.style.opacity = (cutt + speed)/100;
-				obj.style.filter = 'alpha(opacity:'+(cutt + speed)+')';
-			}else{
-				obj.style[att]=cutt+speed+'px';
-			}
-		}
-	},30)
+}
+if(flag){
+	clearInterval(obj.timer)
+	if (fn) {
+		fu();
+	}
+}
+},30)
 }
 
 
 window.onload = function(){
+	/*大广告轮播图 元素*/
   var oDiv = document.getElementById('bannerBox');//获取父元素
   var oPre = getByClass(oDiv,'pre')[0];//获取向上箭头
   var oNext = getByClass(oDiv,'next')[0];//获取向下箭头
@@ -61,7 +69,24 @@ window.onload = function(){
   var aBigLi = oUlBig.getElementsByTagName('li');//获取li
   var oDivSmall = getByClass(oDiv,'wrap_page')[0];//获取点点的父元素
   var aLiSmall = oDivSmall.getElementsByTagName('span');//点点的标签
-  
+	/*大广告轮播图 元素*/
+
+	//导航
+	var navList = document.getElementById('nav_list');
+	var navListLi = getByClass(navList,'nav_list_li');
+	console.log(navListLi);
+	for (var i = 0; i < navListLi.length; i++) {
+		navListLi[i].onmouseover = function () {
+			var navListChildren = this.getElementsByTagName("div")[0];
+			starMove(navListChildren,{opacity:100,height:250},5);
+		};
+		navListLi[i].onmouseout = function () {
+			var navListChildren = this.getElementsByTagName("div")[0];
+			starMove(navListChildren,{opacity:0,height:0},1);
+		}
+
+	};
+	//竖排导航
   //轮播函数
   function tab()
   {
@@ -71,8 +96,8 @@ window.onload = function(){
 	    aLiSmall[i].className = '';
      }
      aLiSmall[now].className = 'wrap_item';
-     startMove(oUlBig,'left',-(now*aBigLi[0].offsetWidth));
-  }
+     starMove(oUlBig,{left:-(now*aBigLi[0].offsetWidth)},5);
+  };
   //定义当前下标
   var now = 0;
   //遍历点点取出下标
@@ -84,8 +109,8 @@ window.onload = function(){
 	  {
 		  now = this.index;
 		  tab();
-	  }
- }
+	  };
+ };
 
 
   oPre.onclick = function()
@@ -96,7 +121,7 @@ window.onload = function(){
 		  now = aBigLi.length;
 	  }
 	   tab();
-  }
+  };
    oNext.onclick = function()
   {
 	   ++now;
@@ -105,15 +130,15 @@ window.onload = function(){
 		  now = 0;
 	  }
 	  tab();
-  }
+  };
   var timer = setInterval(oNext.onclick,3000) //滚动间隔时间设置
   oDiv.onmouseover = function()
   {
 	  clearInterval(timer)
-  }
+  };
    oDiv.onmouseout = function()
   {
 	  timer = setInterval(oNext.onclick,3000) //滚动间隔时间设置
-  }
+  };
 
 }
